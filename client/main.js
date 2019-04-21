2b48ffaf5476d199d33135d4dca0af03b6b10143
zdahmed93 <ahmedzribiahmed@gmail.com>
2019-04-21 21:29:30 +0100

    crud operations done

--- client/main.js
+++ client/main.js
@@ -1,21 +1,52 @@
-import { Tasks } from '../imports/api/tasks'
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
+import { Todos } from '../imports/api/todos'
+
+import { Template } from 'meteor/templating';
 
 
-// const data = [
-//   { text: "Task 1"},
-//   {text: "Task 2"},
-//   {text: "Task 3"}
-// ] 
+console.log(Todos.find({}).fetch());
 
 
-Template.tasks.helpers({
-  tasks() {
-    return Tasks.find({});
+
+Template.main.helpers({
+  'todos': function () {
+    return Todos.find({}, {sort: {createdAt: -1}});
   }
 })
+
+Template.main.events({
+  'click .btn-danger': function (event) {
+    console.log('clicked', event.target.id);
+    if (confirm("Are you sure ?")) {
+      Todos.remove(event.target.id);
+    }
+
+  },
+  'submit #first-form': function (event) {
+    event.preventDefault();
+    console.log(event.target.noteField.value);
+    Todos.insert({
+      text: event.target.noteField.value,
+      createdAt: new Date(),
+      checked: false
+    });
+    event.target.noteField.value = '';
+  },
+  'click .checking': function(event) {
+    console.log(event.target.checked);
+    console.log(event.target.id);
+    Todos.update(event.target.id, {$set: {checked: event.target.checked}})
+    
+  }
+
+})
+
+
+
+
+
+
+
 
 
 
--- server/main.js
+++ server/main.js
@@ -1,5 +1,14 @@
+import '../imports/api/todos';
+import {Todos} from '../imports/api/todos'
+
+Todos.insert({
+  text: 'todo 3',
+  createdAt: new Date()
+})
+
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
 });
